9454cad65090e94964b140708c97f1ff612f4625
pbullhove <peterbullhove@gmail.com>
2022-12-28 12:42:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var connectionString = @"Data Source=R91268WZ;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Database=Test;"; 
+
