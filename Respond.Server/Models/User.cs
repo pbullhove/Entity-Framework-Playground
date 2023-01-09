9c75969cbff0a5f0f1010bfd8af9c7fd2f931a23
pbullhove <peterbullhove@gmail.com>
2023-01-09 11:52:15 +0100

    removed obj

--- Respond.Server/Models/User.cs
+++ Respond.Server/Models/User.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public UserRole Role { get; set; }
         public List<Session> Sessions { get; set; }
         public List<Response> Responses { get; set; }
     }
