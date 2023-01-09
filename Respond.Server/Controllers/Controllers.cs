c31a7b39399d064e9980d9c0330938dd02d88278
pbullhove <peterbullhove@gmail.com>
2023-01-09 12:13:09 +0100

    adding controller

---
+++ Respond.Server/Controllers/Controllers.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReSpond.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public sealed class RepondController : Controller
+    {
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("Respond ok."); 
+        }
+    }
+}
--- Respond.Server/Data/Context.cs
+++ Respond.Server/Data/Context.cs
@@ -10,17 +10,6 @@ namespace Respond.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Course>(g =>
-            {
-                g.HasKey(g => g.Id);
-            });
-            modelBuilder.Entity<Presenter>(m =>
-            {
-                m.HasKey(m => m.Id);
-                m.HasOne(m => m.Genre)
-                 .WithMany()
-                 .IsRequired(true); 
-            });
         }
     }
 }
